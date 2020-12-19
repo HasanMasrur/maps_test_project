9a83300e393da6f24b76f2c9640f5313897e89fe
Hasan Masrur <hasan@Hasans-iMac.local>
2020-12-19 11:20:12 +0600

    added marker , and dialog

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,178 @@
+import 'dart:async';
+import 'dart:typed_data';
+import 'dart:ui';
+import 'package:dio/dio.dart';
+import 'package:flutter/services.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
-void main() {
-  runApp(MyApp());
-}
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Flutter Google Maps Demo',
+      home: MapSample(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class MapSample extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<MapSample> createState() => MapSampleState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class MapSampleState extends State<MapSample> {
+  GlobalKey<ScaffoldState> scaffoldkey = new GlobalKey<ScaffoldState>();
+  // Completer<GoogleMapController> _controller = Completer();
+  Marker marker;
+  Circle circle;
+  StreamSubscription _locationSubscription;
+  GoogleMapController _controller;
+  Location _locationTracker = Location();
+  BitmapDescriptor customIcon;
+  String firstName;
+  String lastName;
+  String email;
+  String avatar;
+  @override
+  void initState() {
+    locatedPosition();
+    getHttp();
+    super.initState();
+  }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  void getHttp() async {
+    try {
+      Response response = await Dio().get("https://reqres.in/api/users/2");
+      print(response.data);
+      print(response.data.length);
+      firstName = response.data['data']['first_name'];
+      lastName = response.data['data']['last_name'];
+      avatar = response.data['data']['avatar'];
+      email = response.data['data']['email'];
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  showdialog() {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            // title: Text('$firstName $lastName'),
+            content: Container(
+              height: 200,
+              child: Column(
+                children: [
+                  Text('Name : $firstName $lastName'),
+                  Text('Email : $email'),
+                  Card(
+                    child: Image.network(avatar),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    elevation: 5,
+                  ),
+                ],
+              ),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('Ok'),
+              )
+            ],
+          );
+        });
+  }
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  Future<Uint8List> getMarker() async {
+    ByteData byteData =
+        await DefaultAssetBundle.of(context).load('assets/car.png');
+    return byteData.buffer.asUint8List();
+  }
+
+  void updateMarkerAndCircle(LocationData position, Uint8List imageData) {
+    LatLng latlag = LatLng(position.latitude, position.longitude);
+
+    this.setState(() {
+      marker = Marker(
+          markerId: MarkerId('home'),
+          position: latlag,
+          rotation: position.heading,
+          draggable: false,
+          // zIndex: 2,
+          flat: true,
+          icon: BitmapDescriptor.fromBytes(imageData),
+          anchor: Offset(0.5, 0.5),
+          onTap: () {
+            showdialog();
+          });
+    });
+  }
+
+  GoogleMapController newGoogleMapConnection;
+
+  Position currentPosition;
+  LatLng latlag;
+  var geoLocation = Geolocator();
+
+  locatedPosition() async {
+    Uint8List imageData = await getMarker();
+    var location = await _locationTracker.getLocation();
+
+    updateMarkerAndCircle(location, imageData);
+
+    if (_locationSubscription != null) {
+      _locationSubscription.cancel();
+    }
+
+    _locationSubscription =
+        _locationTracker.onLocationChanged.listen((newlocation) {
+      if (_controller != null) {
+        _controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+          bearing: 192,
+          target: LatLng(newlocation.latitude, newlocation.longitude),
+          tilt: 0,
+          zoom: 18.00,
+        )));
+        // updateMarkerAndCircle(newlocation);
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return new Scaffold(
+      body: GoogleMap(
+        markers: Set.of((marker != null) ? [marker] : []),
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller = controller;
+          locatedPosition();
+        },
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      //   floatingActionButton: FloatingActionButton.extended(
+      //     onPressed: () {
+      //       return locatedPosition();
+      //     },
+      //     label: Text('your corrent location'),
+      //     icon: Icon(Icons.location_searching),
+      //   ),
     );
   }
 }
